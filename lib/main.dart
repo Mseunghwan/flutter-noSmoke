30ee71be7eaae008d225ccdf71a640ec77ddc9fe
Mseunghwan <minccc42710@gmail.com>
2024-11-07 21:06:51 +0900

    main.dart 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/LoginPage.dart';
+import 'firebase_options.dart';  // 자동 생성된 firebase_options 파일 import
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
